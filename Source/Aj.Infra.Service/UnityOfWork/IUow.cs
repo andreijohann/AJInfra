8326742be5f68f37206c0353232eacac28cf024f
Andrei Johann <andrei_johann@yahoo.com>
2022-01-30 12:34:20 +1000

    Service layer - Definition of interfaces fos Unit Of Work pattern

---
+++ Source/Aj.Infra.Service/UnityOfWork/IUow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aj.Infra.Service.UnityOfWork
+{
+    /// <summary>
+    /// The “Unit Of Work” pattern maintains a list of objects affected by a transaction, coordinates writing changes, and handles potential concurrency issues.
+    /// </summary>
+    public interface IUow : IDisposable
+    {
+        /// <summary>
+        /// Signals the start of the transaction
+        /// </summary>
+        void Begin();
+
+        /// <summary>
+        /// Saves changes in context (SaveChanges).
+        /// </summary>
+        void Commit();
+
+        /// <summary>
+        /// Discards changes in context (DiscardChanges).
+        /// </summary>
+        void Rollback();
+
+    }
+}
---
+++ Source/Aj.Infra.Service/UnityOfWork/IUowWithIntermediateSavings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aj.Infra.Service.UnityOfWork
+{
+    /// <summary>
+    /// Unit of Work interface with possibility of intermediate saves.
+    /// </summary>
+    public interface IUowWithIntermediateSavings: IUow
+    {
+        /// <summary>
+        /// Saves the changes made so far.
+        /// </summary>
+        void Save();
+    }
+}
