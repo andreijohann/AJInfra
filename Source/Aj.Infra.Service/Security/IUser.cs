b5524091e4cf2869ef1daaf413a5ca95ec0f0eef
Andrei Johann <andrei_johann@yahoo.com>
2022-01-30 21:22:01 +1000

    Service layer - Security - Interface definitions

---
+++ Source/Aj.Infra.Service/Security/IUser.cs
@@ -0,0 +1,14 @@
+﻿namespace Aj.Infra.Service.Security
+{
+    /// <summary>
+    /// Interface that represents a minimal set of information about the application user
+    /// </summary>
+    public interface IUser
+    {
+        long Id { get; set; }
+        string Name { get; set; }
+        string Email { get; set; }
+        string Organization { get; set; }
+        string IpAddress { get; set; }
+    }
+}
---
+++ Source/Aj.Infra.Service/Security/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using Aj.Infra.Dto;
+
+namespace Aj.Infra.Service.Security
+{
+    /// <summary>
+    /// Interface that represents a minimal set of actions on the application user
+    /// </summary>
+    public interface IUserService
+    {
+        IUser GetCurrentUser();
+        ResultDto hasPermission(IUser user, string sisObjectAction);
+        ResultDto hasPermission(string sisObjectAction);
+        ResultDto hasRole(IUser user, string sisRole);
+        ResultDto hasRole(string sisRole);
+    }
+}
