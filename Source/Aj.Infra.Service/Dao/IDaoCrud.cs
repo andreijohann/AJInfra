aa2b4c4bc76bd8b19379e4477d74ce980e887876
Andrei Johann <andrei_johann@yahoo.com>
2022-01-30 13:39:55 +1000

    Service layer - Data Access objects - Basic crud interfaces

---
+++ Source/Aj.Infra.Service/Dao/IDaoCrud.cs
@@ -0,0 +1,14 @@
+﻿using Aj.Infra.Dto;
+using System;
+using System.Linq.Expressions;
+
+namespace Aj.Infra.Service.Dao
+{
+    public interface IDaoCrud<TPersistEnt, TId> : IDaoCrudBase<TPersistEnt, TId>
+        where TPersistEnt : class, IPersistenceEnt<TId>
+    {
+        TPersistEnt GetById(TId id);
+
+        TPersistEnt Get(Expression<Func<TPersistEnt, bool>> filter, params Expression<Func<TPersistEnt, object>>[] relatedProp);
+    }
+}
---
+++ Source/Aj.Infra.Service/Dao/IDaoCrudBase.cs
@@ -0,0 +1,12 @@
+﻿using Aj.Infra.Dto;
+
+namespace Aj.Infra.Service.Dao
+{
+    public interface IDaoCrudBase<TPersistEnt, TId>
+        where TPersistEnt : class, IPersistenceEnt<TId>
+    {
+        void Insert(TPersistEnt entity);
+        void Update(TPersistEnt entity);
+        void Delete(TPersistEnt entity);
+    }
+}
