ca057941ac5a982e0fd7dcffad7091cb746b48a6
Andrei Johann <andrei_johann@yahoo.com>
2022-01-30 18:36:48 +1000

    Interface for basic CRUD operations in the service layer

---
+++ Source/Aj.Infra.Service/Services/IServiceCrud.cs
@@ -0,0 +1,17 @@
+﻿using Aj.Infra.Dto;
+
+namespace Aj.Infra.Service.Services
+{
+    /// <summary>
+    /// Interface for basic CRUD operations in the service layer
+    /// </summary>
+    /// <typeparam name="TDto">Type of Dto object</typeparam>
+    /// <typeparam name="TId">Type of the ID of the object</typeparam>
+    public interface IServiceCrud<TDto, TId> where TDto : class
+    {
+        CreateResultDto<TId> Insert(TDto dot);
+        ResultDto Update(TId id, TDto dot);
+        ResultDto Delete(TId id);
+        QueryResultDto<TDto> Get(TId id);
+    }
+}
